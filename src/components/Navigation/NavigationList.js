6790d9fa3db23452d651318ed431889b79897759
suthargk <gaurav.suthar161@gmail.com>
2022-05-16 17:33:18 +0530

    styled scroll for windows

--- src/components/Navigation/NavigationList.js
+++ src/components/Navigation/NavigationList.js
@@ -8,7 +8,7 @@ const NavigationList = memo(({
 }) => {
   return (
     <div className="h-20 p-4 pb-0 lg:h-auto">
-      <ul className="box-content lg:box-border pb-2 mt-4 space-x-2 lg:space-x-0 lg:space-y-6 h-full overflow-x-scroll lg:overscroll-x-none overflow-y-hidden whitespace-nowrap w-full">
+      <ul className="box-content lg:box-border pb-2 mt-4 space-x-2 lg:space-x-0 lg:space-y-6 h-full overflow-x-scroll lg:overflow- overflow-y-hidden lg:overflow-hidden whitespace-nowrap w-full">
         {data?.getPlaylists?.map((playList) => (
           <Navigation
             key={playList.id}
